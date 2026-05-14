65bb1656859f2526861e2abe121d2354ff9502e3
Rafael Haenel <rhaenel@quera.com>
2026-05-14 09:06:55 -0400

    Add circuits

--- demos/msc.py
+++ demos/msc.py
@@ -12,3 +12,17 @@ n = 10_000  # On GPU, increase batch size to fully utilize VRAM
 sampler.sample(shots=n, batch_size=n)
 end_time = time.perf_counter()
 print(f"Time per shot: {(end_time - start_time) / n * 1e6:.2f} microseconds")
+
+# %%
+c = tsim.Circuit.from_file("../circuits/msd_5.stim")
+sampler = c.compile_detector_sampler(strategy="cat5")  # Fastest with default strategy (cat5)
+print(sampler)
+samples = sampler.sample(shots=100_000, batch_size=100_000)
+
+
+#%%
+for d in [5, 7, 9]:
+    c = tsim.Circuit.from_file(f"../circuits/star_d{d}.stim")
+    sampler = c.compile_detector_sampler(strategy="cat5")  # Fastest with default strategy (cat5)
+    print(sampler)
+    samples = sampler.sample(shots=1_000, batch_size=1_000)
